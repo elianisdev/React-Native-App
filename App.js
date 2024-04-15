9ee76e2c1188e78e26aa39637fd4408cda192957
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-04-14 20:33:08 -0500

    Create app component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import{ Text, View } from 'react-native';
-
+import{ Text, View, StyleSheet, Image } from 'react-native';
+//asi se importaria una imagen si estuviera guardada en la carpeta assets y la llamo en source entre llaves con el nombre de la variable
+//import image from './assets/mia.jpg';
 const App = () => {
   return (
-  <View style={{flex:1 , justifyContent: "center" , alignItems: "center"}}>
-    <Text style={{fontSize: 30}}> Hello World </Text>
+  <View style={styles.container}>
+    <Text style={styles.title}> Hello World </Text>
+    <Image
+      //source={image}
+    source={{ uri: "https://picsum.photos/200/200"}}
+    style={styles.img}
+    />
   </View>
 );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex:1 ,
+    justifyContent: "center" ,
+    alignItems: "center",
+    backgroundColor: "#87ccea"
+  },
+  title: { fontSize: 30 } ,
+    img: { width: 200, height: 200, borderRadius: 100}
+});
 export default App;
